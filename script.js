f2d9ba86a224cfb120cfccbe1336b5d7b280a357
1qasimabidfreelancer <1qasim.freelancer@gmail.com>
2024-06-21 14:14:43 +0500

    JavaScript Variables

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-alert("Hello world");
+console.log("Hey this is tutorial 55")
 
-console.log("cod is running...")
-console.log("code is also running...")
-console.log ("cod is looking like a wow... ")
+let a = 5;
+let b = 6;
+let c = "Qasim";
+let _a = "Usman"
+// var 55a = "Rohan";// Not Allowed
 
-var a = prompt("Enter your number")
-var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
-
-if(isTrue){
-console.log("computer is blasting")
-}
-
+//console.log (a + b + 8 )
+//console.log(typeof a, typeof b, typeof c);
 {
-    console.log("computer is not blasting")
+    //let a = 66;
+    console.log(a)
 }
 
-console.log ("your number is " + a)
+console.log(a)
 
-document.title = "Hay I Am Good"
-document.body.style.backgroundColor = "Grey"
+//const a1 = 6;
+//a1 = a1 + 1;   // not Allowed  because a1 is constant
+
+
+let x = "Qasim Bhi";
+let y = 22;
+let z = 355;
+const p = true;
+let q = undefined;
+let r = null;
+
+
+console.log(x, y, z, p, q, r,)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r,)
+
+let g = {
+    "Name": "Qasim",
+    "job code": 5600,
+    "Qasim jony boy": true,
+}
+
+console.log(g);
+g.salary = "100crores";
+console.log(g);
+g.salary = "500crores";
+console.log(g);
+
